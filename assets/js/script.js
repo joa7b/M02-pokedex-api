233727a817f0581a51627dab8f67c0714cede90c
joa7b <joa7@protonmail.com>
2022-07-21 20:59:07 -0300

    prof ver

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,9 @@
 let page = 0;
 
 async function allPokemon() {
-  const getAllPokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/?offset=${page}&limit=50`);
+  const getAllPokemon = await fetch(
+    `https://pokeapi.co/api/v2/pokemon/?offset=${page}&limit=50`
+  );
   const data = await getAllPokemon.json();
 
   data.results.forEach(async function (item) {
@@ -12,53 +14,73 @@ async function allPokemon() {
       `https://pokeapi.co/api/v2/pokemon-species/${pokeData.order}`
     );
     const dataText = await pokeText.json();
-    let count = Math.floor(Math.random() * 50);
+    
     let pokeDescription;
 
-    while(dataText.flavor_text_entries[count].language.name != "en") {
-      count = Math.floor(Math.random() * 50);
+    for(let i = 0; i<500;i++){
+      if(dataText.flavor_text_entries[i].language.name === "en") {
+        pokeDescription = dataText.flavor_text_entries[i].flavor_text
+        break;
+      }
+      
     }
-
-    pokeDescription = dataText.flavor_text_entries[count].flavor_text;
 
     for (let i = 0; i < pokeData.types.length; i++) {
       arrayTypes.push(pokeData.types[i].type.name);
     }
 
+    let count = 0
+    let idteste = pokeData.id;
+
     document.querySelector("#pokeContainer").insertAdjacentHTML(
       "beforeend",
       `
         <div class="pokeCards flex-box">
-
-            <img src="${pokeData.sprites.other.home.front_default}" alt="">
-
-            <div class="pokeCardInfo flex-box">
-                <h2 class="pokeName">${pokeData.name}</h2>
-                <span class="pokeOrder">Order: ${pokeData.order}</span>
-                <span class="pokeType">Type: ${arrayTypes}</span>
-                <p class="pokeDescription">${pokeDescription}</p>
+          <div class="pokeCards2 flex-box">
+            <div class="imgContainer" onclick="baloonImg()">
+              <img src="${pokeData.sprites.other.home.front_default}" alt="" class="pokeImg">
             </div>
 
+            <div class="pokeCardInfo flex-box">
+              <div class="pokeNID flex-box">
+                <h2 class="pokeName">${pokeData.name}</h2>
+                <span class="pokeOrder">ID: ${pokeData.id}</span>
+              </div>
+                <span class="pokeType">${arrayTypes}</span>
+                <p class="pokeDescription">${pokeDescription}</p>  
+            </div>
+          </div>
+          <button class="btnDetail" onclick="details()">Detalhes</button>
+          <div class="pokeCards teste " id="${pokeData.id}">
+            <p>HP: ${pokeData.stats[0].base_stat}</p>
+            <p>Attack: ${pokeData.stats[0].base_stat}</p>
+            <p>Defense: ${pokeData.stats[0].base_stat}</p>
+            <p>Special Attack: ${pokeData.stats[0].base_stat}</p>
+            <p>Special Defense: ${pokeData.stats[0].base_stat}</p>
+            <p>Special Defense: ${pokeData.stats[0].base_stat}</p>
+          </div>
         </div>
         `
     );
   });
+
+
 }
 
 allPokemon();
 
 //paginação simples
-function viewMore(){
+function viewMore() {
   page += 50;
   allPokemon();
 }
 
 //paginação infinita
-window.addEventListener("scroll", function (){
-  const {scrollTop, scrollHeight, clientHeight } = document.documentElement;
+window.addEventListener("scroll", function () {
+  const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
 
-  if(scrollTop + clientHeight >= scrollHeight - 300){
-      viewMore();
+  if (scrollTop + clientHeight >= scrollHeight - 300) {
+    viewMore();
   }
 });
 
@@ -69,16 +91,71 @@ pokeInput.addEventListener("keyup", () => {
   const allH2 = document.querySelectorAll(".pokeName");
   const allCards = document.querySelectorAll(".pokeCards");
 
-  for(let n of allH2) {
+  for (let n of allH2) {
     // console.log(n.innerText);
-  
-  if(pokeInput.value === "") {
-    n.parentElement.parentElement.style.display = "flex"
-  }else if(n.innerText.toLowerCase().includes(pokeInput.value.toLowerCase())){
-      n.parentElement.parentElement.style.display = "flex"
-    }else{
-      n.parentElement.parentElement.style.display = "none";
+
+    if (pokeInput.value === "") {
+      n.parentElement.parentElement.style.display = "flex";
+    } else if (
+      n.innerText.toLowerCase().includes(pokeInput.value.toLowerCase())
+    ) {
+      n.parentElement.parentElement.style.display = "flex";
+    } else {
+      n.parentElement.parentElement.parentElement.parentElement.style.display = "none";
     }
   }
+});
 
-})
+// const teste = document.querySelectorAll(".teste");
+
+// forEach(teste) {
+
+// }
+
+function details() {
+  const teste = document.querySelectorAll(".teste");
+
+  for(let n of teste) {
+    console.log(n.innerText)
+    n.style.display = "block";
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function baloonImg() {
+//   let pokeImg = document.querySelectorAll(".imgContainer");
+
+//   for(let n of pokeImg){
+//     n.addEventListener("click", () => {
+
+//       if(n.childElementCount === 1 || n.querySelector(".pokeBaloon").style.display === "none") {
+//         n.insertAdjacentHTML("afterbegin",
+//         `<div class="pokeBaloon">
+//           <img src="./assets/images/1373094.svg" alt="">
+//         </div>`
+//         )
+//       }else {
+//         n.querySelector(".pokeBaloon").style.display = "none";
+//       }
+      
+      
+//     });
+//   }
+
+// }
+
+
+
